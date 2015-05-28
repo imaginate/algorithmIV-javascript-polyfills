0945c84b1874097fcde19b78fb045a4101bcef3d
Adam Smith <adamsmith@algorithmiv.com>
2015-05-27 18:35:02 -0700

    Updated the Skeleton

--- src/pre-compiled-cure/skeleton.js
+++ src/pre-compiled-cure/skeleton.js
@@ -2,28 +2,24 @@
 
 /**
  * -----------------------------------------------------------------------------
- * Algorithm IV JavaScript Polyfills (v0.0.2)
+ * Cure.js (v0.0.2)
  * -----------------------------------------------------------------------------
- * @file Algorithm IV's JavaScript polyfills help ensure that our projects are
- *   cross-browser compatible. At the moment they only include selected
- *   polyfills that are used in our code base. The only native functionality
- *   that is completely polyfilled is the
- *   [Console]{@link https://github.com/imaginate/algorithmIV-javascript-polyfills/blob/master/src/pre-compiled-parts/polyfills/console.js}.
- * @module aIVPolyfills
+ * @file Cure.js is a collection of JavaScript and DOM polyfills that help
+ *   ensure that your web development is cross-browser compatible. At the moment
+ *   they only include a small selection of polyfills needed to support
+ *   [imaginate]{@link https://github.com/imaginate}'s other projects. The only
+ *   native object's functionality that has been completely polyfilled is the
+ *   [Console]{@link https://github.com/imaginate/cure/blob/master/src/pre-compiled-cure/polyfills/console.js}
+ *   object.
+ * @module Cure
  * @version 0.0.2
- * @author Adam Smith ({@link adamsmith@youlum.com})
- * @copyright 2015 Adam A Smith ([github.com/imaginate]{@link https://github.com/imaginate})
- * @license The Apache License ([algorithmiv.com/docs/license]{@link http://algorithmiv.com/docs/license})
- * @desc More details about aIV's polyfills:
- * <ol>
- *   <li>annotations: 
- *       [See Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
- *       and [See JSDoc3]{@link http://usejsdoc.org/}
- *   </li>
- *   <li>contributing: 
- *       [See our guideline]{@link https://github.com/imaginate/algorithmIV-javascript-polyfills/blob/master/CONTRIBUTING.md}
- *   </li>
- * </ol>
+ * @author Adam Smith adamsmith@algorithmiv.com
+ * @copyright 2015 Adam A Smith [github.com/imaginate]{@link https://github.com/imaginate}
+ * @license The Apache License [algorithmiv.com/cure/license]{@link http://algorithmiv.com/cure/license}
+ * @see [Contributing Guide]{@link https://github.com/imaginate/cure/blob/master/CONTRIBUTING.md}
+ * Annotations:
+ * @see [JSDoc3]{@link http://usejsdoc.org/}
+ * @see [Closure Compiler specific JSDoc]{@link https://developers.google.com/closure/compiler/docs/js-for-compiler}
  */
 
 /**
@@ -48,7 +44,7 @@
 // insert-json3
 
 ////////////////////////////////////////////////////////////////////////////////
-// The JavaScript Polyfills
+// The Cure Polyfills
 ////////////////////////////////////////////////////////////////////////////////
 
 ;(function(window, document, undefined) {
